c42e4c777e2153073a88b296575761b1a5e95752
MacroModel <macromodel@trajectronix.onmicrosoft.com>
2024-01-05 18:20:39 +0800

    add tjmalloc

---
+++ tjmalloc/test/tjmalloc_compile.cpp
@@ -0,0 +1,10 @@
+﻿#include <fast_io/fast_io.h>
+#include <tjmalloc/platform.h>
+
+struct A {
+	inline static constexpr ::std::size_t page_size{};
+};
+
+int main() {
+	constexpr auto i = tjmalloc::is_platform<A>;
+}
